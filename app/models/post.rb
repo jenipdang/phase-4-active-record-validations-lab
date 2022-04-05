8957e8fa351bb9617fad117a6bac08f1afb67db7
Jeni <jenipdang@yahoo.com>
2022-04-04 19:05:42 -0700

    commit tree ee886806f6fc1e30050ae5c00d819c6d53d251bd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)} 
+    validate :be_valid?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def be_valid?
+        if CLICKBAIT_PATTERNS.none? { |pattern| pattern.match title }
+            errors.add(:title, "Must be clickbaity-y")
+        end
+    end
+
 end
